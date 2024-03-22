625e0cced9c1a9b3d41c3caddfd1e9f19f6a956d
Crowdbotics <team@crowdbotics.com>
2024-03-22 07:32:34 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fast-trax-47207",
+  url: "https://fast-trax-47207.botics.co",
+  api: "https://fast-trax-47207.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
